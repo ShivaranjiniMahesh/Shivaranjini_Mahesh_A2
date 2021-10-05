1407a79d224dfadc8cc10c523d520ac45a8a0820
ShivaranjiniMahesh <shivaranjinimahesh060199@gmail.com>
2021-10-05 17:51:55 -0400

    containing the complete rectangle class

---
+++ SQAT-Assignment2/SQAT-Assignment2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQAT_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            a.Begin();
+            Console.ReadLine();
+        }
+        public void Begin()
+        {
+            
+            Console.Write("Enter the length of the rectangle : ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle : ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle rect = new Rectangle(length,width);
+            Console.WriteLine("Perimeter of rectangle is " + rect.GetPerimeter());
+             Console.WriteLine("Area of rectangle is" + rect.GetArea());
+            Console.WriteLine(" Rectangle of Length {0} and Width {1} will result in  Perimeter {2} and Area {3}  ", rect.GetLength(), rect.GetWidth(), rect.GetPerimeter(), rect.GetArea());
+            Console.WriteLine("-------");
+
+            Rectangle rect1 = new Rectangle(5,10);
+            Console.WriteLine("Perimeter of rectangle is : " + rect1.GetPerimeter());
+            Console.WriteLine("Area of rectangle is: " + rect1.GetArea());
+            Console.WriteLine("Rectangle of Length {0} and Width {1} will result in  Perimeter {2} and Area {3} ", rect1.GetLength(), rect1.GetWidth(), rect1.GetPerimeter(), rect1.GetArea());
+            Console.WriteLine("-------");
+
+            Rectangle rect2 = new Rectangle( );
+            Console.WriteLine("Perimeter of rectangle is : " + rect2.GetPerimeter());
+            Console.WriteLine("Area of rectangle is: " + rect2.GetArea());
+            Console.WriteLine("Rectangle of Length {0} and Width {1} will result in  Perimeter {2} and Area {3}  ", rect2.GetLength(), rect2.GetWidth(), rect2.GetPerimeter(), rect2.GetArea());
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SQAT-Assignment2/SQAT-Assignment2/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQAT_Assignment2
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public  Rectangle ()
+        { 
+            length = 1;
+            width = 1; 
+        }
+
+        public Rectangle(int l , int w)
+        {
+            length =l;
+            width = w;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int SetLength(int l) => length = l; 
+
+        public int GetWidth()
+        {
+            return width; 
+        }
+
+        public  int SetWidth(int w) => width = w; 
+
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        public int GetArea()
+        {
+            return length * width; 
+        }
+
+
+}
+}
